870cfc596e18201d693acf5087a833d89a1eefb5
Breno Oliveira <breno.oliveira@avancoinfo.com.br>
2020-02-14 17:19:22 -0300

    tslint

--- src/modulos/objeto/objeto.controller.ts
+++ src/modulos/objeto/objeto.controller.ts
@@ -1,15 +1,14 @@
 import { Controller } from '@nestjs/common';
-import { CrudService } from '../../core/crud/crud.service';
 import { CrudController } from '../../core/crud/crud.controller';
 import { ObjetoEntity } from './objeto.entity';
 import { ObjetoService } from './objeto.service';
-// import { ApiUseTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('objetos')
-// @ApiUseTags('Objetos')
+@ApiTags('Objetos')
 export class ObjetoController extends CrudController<ObjetoEntity> {
     constructor(
-        private objetoService: ObjetoService
+        private objetoService: ObjetoService,
     ) {
         super(objetoService);
     }
--- src/modulos/pessoa/pessoa.controller.ts
+++ src/modulos/pessoa/pessoa.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Param, Get, Post, Body, ParseIntPipe, Query, Request } from "@nestjs/common";
-// import { ApiUseTags } from "@nestjs/swagger";
-import { PessoaService } from "./pessoa.service";
-import { Pessoa } from "./pessoa.entity";
-// import { CreatePersonDTO } from "./pessoa.dto";
-import { CrudController } from "../../core/crud/crud.controller";
+import { Controller, Param, Get, Post, Body, ParseIntPipe, Query, Request } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { PessoaService } from './pessoa.service';
+import { Pessoa } from './pessoa.entity';
+// import { CreatePersonDTO } from './pessoa.dto';
+import { CrudController } from '../../core/crud/crud.controller';
 
 @Controller('pessoas')
-// @ApiUseTags('Pessoas')
+@ApiTags('Pessoas')
 export class PessoaController extends CrudController<Pessoa> {
     constructor(
         private pessoaService: PessoaService,
-    ) { 
+    ) {
         super(pessoaService);
     }
 
